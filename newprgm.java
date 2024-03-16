6c7f59c48a65ac5336c7c4ad98b5bb095f32a26e
swapnil_ranjan <swapnilranjan181200@gmail.com>
2024-03-16 16:42:10 +0530

    new

---
+++ newprgm.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class newprgm {
+ 
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0){
+		    int N  = sc.nextInt();
+		    if(N%3==0)
+		    System.out.println(0);
+		    else 
+		    System.out.println(3 - N%3);
+		}
+	}
+
+
+}
+
+	
