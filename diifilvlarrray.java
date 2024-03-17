11296bf8eef7aae4a7c766cf09356b9962f83a9d
swapnil_ranjan <swapnilranjan181200@gmail.com>
2024-03-17 15:26:30 +0530

    add

---
+++ diifilvlarrray.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class diifilvlarrray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        while (t-- > 0) {
+            int n = scanner.nextInt();
+            int[] d = new int[n];
+            
+            for (int i = 0; i < n - 1; i++) {
+                d[i] = scanner.nextInt();
+                if (d[i] < d[i + 1]) {
+                    System.out.println("YES");
+                } else {
+                    System.out.println("NO");
+                }
+            }
+        }
+    }
+
+}
+   
