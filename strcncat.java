7c1dedd588c2cee38a777bc4dcfe0b3c36531405
swapnil_ranjan <swapnilranjan181200@gmail.com>
2024-03-19 10:41:10 +0530

    new

---
+++ strcncat.java
@@ -0,0 +1,8 @@
+public class strcncat {
+    public static void main(String[] args) {
+        String fname = "Swapnil";
+        String lname = "Ranjan";
+        String fullname =fname+" "+lname;
+        System.out.println(fullname);
+    }
+}
