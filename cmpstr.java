835f165434db8e5266d240bce3b1ec609b8e2b4a
swapnil_ranjan <swapnilranjan181200@gmail.com>
2024-03-19 10:50:29 +0530

    new

---
+++ cmpstr.java
@@ -0,0 +1,10 @@
+public class cmpstr {
+    public static void main(String[] args) {
+            String fname = "Swapnil";
+            String lname = "Ranjan";
+            if(fname.compareTo(lname)==0){
+                System.out.println("Yes its Equal");
+            }
+            else System.out.println("NO");
+        }
+    }
---
+++ lengthstr.java
@@ -0,0 +1,9 @@
+public class lengthstr {
+    public static void main(String[] args) {
+        String fname = "Swapnil";
+        String lname = "Ranjan";
+        String fullname =fname+" "+lname;
+        System.out.println(fullname);
+        System.out.println(fullname.length());
+    }
+}
