7348529cf137666edff0d2665eb9dab6a695dfc6
swapnil_ranjan <swapnilranjan181200@gmail.com>
2024-03-22 14:59:35 +0530

    copy

---
+++ copy.java
@@ -0,0 +1,13 @@
+public class copy {
+    public static void main(String[] args) {
+        String fname = "Swapnil";
+        String lname = "Ranjan";
+        String fullname =fname+" "+lname;
+        System.out.println(fullname);
+        
+
+        for(int i=0;i<fullname.length();i++){
+            System.out.println(fullname.charAt(i));
+        }
+    }
+}
