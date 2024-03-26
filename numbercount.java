b3fa127a97290cb5d2ca69b342f08976fc86b9af
swapnil_ranjan <swapnilranjan181200@gmail.com>
2024-03-26 18:13:28 +0530

    build

---
+++ numbercount.java
@@ -0,0 +1,11 @@
+public class numbercount {
+    public static void main(String[] args) {
+        int n=223;
+        int count=0;
+        while(n>0){
+            n=n/10;
+            count++;
+        }
+        System.out.println(count);
+    }
+}
