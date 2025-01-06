9a94ff45fec8be789884afebdd8d320f572052e4
swapnil_ranjan <swapnilranjan181200@gmail.com>
2025-01-06 19:01:03 +0530

    update 0.1

--- highestnumberfromagiven_num.java
+++ highestnumberfromagiven_num.java
@@ -2,16 +2,25 @@ import java.util.*;
 
 public class highestnumberfromagiven_num {
     public static void main(String[] args) {
-        int num = 123;
+        int num = 28194812;
 
-        String str = String.valueOf(num);
-        System.out.println(str);
-        char arr[] = str.toCharArray();
+        String str = String.valueOf(num); // convert int to string
+        System.out.println(str);  
+        char arr[] = str.toCharArray();  //converting string to array
 
         for(int i=0; i<arr.length; i++){
-            System.out.println(arr[i]);
+            System.out.println(arr[i]);   //printing the array
         }
-        Arrays.sort(arr);
+        Arrays.sort(arr);   // sorting the array arr
         System.out.println(Arrays.toString(arr));
+
+        String strr = new String(arr);   //converting array back to string for reversing
+        System.out.println(strr);
+
+        String rev = "";   //taken an empty string so i can put my values in here
+        for(int i=strr.length()-1; i>=0; i--){   
+        rev = rev + strr.charAt(i);   //entering the rev values one by one
     }
+    System.out.println(rev);   // printing the output
+}
 }
