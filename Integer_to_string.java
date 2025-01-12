879ba781c04e5fb1c723c0292cd2263c59d14e3b
swapnil_ranjan <swapnilranjan181200@gmail.com>
2025-01-12 12:33:42 +0530

    updated 12jan

---
+++ Integer_to_string.java
@@ -0,0 +1,7 @@
+public class Integer_to_string {
+    public static void main(String[] args) {
+        int num = 1222;
+        String str = String.valueOf(num);
+        System.out.println(str);
+    }
+}
